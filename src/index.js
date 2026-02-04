20a6b3e36c59615db7a2f96a22520dfcaf149086
ephraim64 <tettehephraim.64@gmail.com>
2026-02-04 00:40:03 +0000

    implement matches POST  and GET routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import express from 'express';
+import { matchRouter } from './routes/matchesRoute.js';
 
 const app = express();
 const port = 8000;
 
-// Use JSON middleware
 app.use(express.json());
 
-// Root GET route that returns a short message
 app.get('/', (req, res) => {
   res.send('Welcome to the Express.js server!');
 });
 
-// Log URL when the server starts
+
+app.use('/matches', matchRouter)
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
